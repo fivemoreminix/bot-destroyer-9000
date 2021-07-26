bd864f0b0509a0c35b10b29fa607c43ccf29259a
Luke I. Wilson <thelukaswils@gmail.com>
2021-07-26 13:47:36 -0500

    Add logging and fix code

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bot Destroyer 9000
+
+This bot was created out of a need to block raid attempts by scammers on Discord. They'd always join the server in
+batches of 10 or 20 users, so it is relatively easy to counter. Use bots to fight bots!
+
+## Enable Logging
+
+You enable logging through environment variables.
+
+Best common usage:
+```
+RUST_LOG=bot_destroyer_9000=info bot-destroyer-9000.exe
+```
+
+See [env_logger](https://crates.io/crates/env_logger) for more info.
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,92 @@ use std::env;
 
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
+    model::prelude::*,
     prelude::*,
 };
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use std::collections::HashMap;
+use serenity::client::bridge::gateway::GatewayIntents;
+use log::{info, warn, error};
+
+const DEFAULT_SEARCH_DUR: Duration = Duration::from_secs(60); // 1 minute
+const MIN_USERS_JOIN: usize = 2; // minimum of users to join in given duration to trigger actions
+const DEFAULT_ACTION: Action = Action::Kick;
+
+#[derive(Debug)]
+enum Action {
+    Kick,
+    Ban,
+}
+
+// Captures a moment in time that a user joins the guild.
+struct UserJoinMoment {
+    instant: Instant,
+    member: Member,
+    action_taken: bool, // Whether this member has already been kicked / banned
+}
+
+struct GuildJoinsWatcher;
+
+impl TypeMapKey for GuildJoinsWatcher {
+    type Value = Arc<RwLock<HashMap<GuildId, Vec<UserJoinMoment>>>>;
+}
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
+    // Called when a user joins the server.
+    async fn guild_member_addition(&self, _ctx: Context, _guild_id: GuildId, _new_member: Member) {
+        let guilds_lock = {
+            let data_read = _ctx.data.read().await;
+            data_read.get::<GuildJoinsWatcher>().expect("Expected GuildJoinsWatcher in TypeMap").clone()
+        };
+
+        let mut guilds_map = guilds_lock.write().await;
+        let joins = guilds_map.entry(_guild_id).or_insert(Vec::new());
+        let now = Instant::now();
+        joins.push(UserJoinMoment{instant: now, member: _new_member, action_taken: false});
+
+        // Clean up any values that are out of the required time span
+        joins.retain(|v| now - v.instant <= DEFAULT_SEARCH_DUR);
+        info!("{} users have joined within the search duration", joins.len());
+
+        // Check if we can start banning users
+        if joins.len() >= MIN_USERS_JOIN {
+            info!("Stopping a raid in server {:?}", _guild_id);
+            // Yeah, looks like a raid. Time to take action.
+            for join in joins.iter_mut() {
+                if !join.action_taken {
+                    let _ = match DEFAULT_ACTION {
+                        Action::Kick => join.member.kick_with_reason(_ctx.http.clone(), "Suspected bot; performing raid defense").await,
+                        Action::Ban => join.member.ban_with_reason(_ctx.http.clone(), 7, "Suspected bot; performing raid defense").await,
+                    };
+                    join.action_taken = true;
+                    info!("{:?} {}", DEFAULT_ACTION, join.member);
+                }
+            }
+        }
+    }
+
+    // Called when a member who has previously joined is kicked, banned, or leaves.
+    async fn guild_member_removal(&self, _ctx: Context, _guild_id: GuildId, _kicked: User) {
+        let guilds_lock = {
+            let data_read = _ctx.data.read().await;
+            data_read.get::<GuildJoinsWatcher>().expect("Expected GuildJoinsWatcher in TypeMap").clone()
+        };
+
+        let mut guilds_map = guilds_lock.write().await;
+        let joins = guilds_map.entry(_guild_id).or_insert(Vec::new());
+        // Remove the member from the list if they are NOT recently kicked by raid-blocking
+        joins.retain(|v| v.action_taken || v.member.user != _kicked);
+    }
+
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
+        if msg.mentions_user_id(ctx.http.get_current_user().await.expect("Could not find my own user").id) {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "That's me!").await {
+                warn!("Error sending message: {:?}", why);
             }
         }
     }
@@ -31,29 +96,34 @@ impl EventHandler for Handler {
     // shard is booted, and a READY payload is sent by Discord. This payload
     // contains data like the current user's guild Ids, current user data,
     // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+        ctx.set_activity(Activity::watching("for robots")).await;
     }
 }
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+
     // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut client =
-        Client::builder(&token).event_handler(Handler).await.expect("Err creating client");
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .intents(GatewayIntents::GUILD_MESSAGES | GatewayIntents::GUILD_MEMBERS) // events we want to receive
+        .await.expect("Error creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<GuildJoinsWatcher>(Arc::new(RwLock::new(HashMap::new())));
+    }
 
     // Finally, start a single shard, and start listening to events.
     //
     // Shards will automatically attempt to reconnect, and will perform
     // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
+        error!("Client error: {:?}", why);
     }
 }
